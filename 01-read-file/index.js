7519b43ebe0f671bb7782cd36f6a5be754c071e1
valeriapavlova <vapavlovalera@gmail.com>
2025-01-19 21:26:39 +0100

    feat: add script to read and output text.txt content

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
+// File path
+const filePath = path.join(__dirname, 'text.txt');
+
+// Read file
+const readStream = fs.createReadStream(filePath, 'utf8');
+
+// Read file data
+readStream.on('data', (chunk) => {
+  process.stdout.write(chunk);
+});
