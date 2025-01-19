76c60816efc1007c64ace52f7afd7680072d2ee5
valeriapavlova <vapavlovalera@gmail.com>
2025-01-20 00:03:24 +0100

    fix: correct indentation in index.js in 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,19 +10,24 @@ async function displayFileInfo() {
     const files = await fs.readdir(folderPath, { withFileTypes: true });
 
     for (const file of files) {
-        if (file.isFile()) {
-          const filePath = path.join(folderPath, file.name);
-          //Get file stats
-          const stats = await fs.stat(filePath);
-          //Extract file name and extension
-          const fileName = path.parse(filePath.name).name;
-          const fileExt = path.extname(filePath.name).slice(1);
-            //Get file size in KB
-          const fileSizeKB = (stats.size / 1024).toFixed(2);
+      if (file.isFile()) {
+        const filePath = path.join(folderPath, file.name);
+        //Get file stats
+        const stats = await fs.stat(filePath);
+        //Extract file name and extension
+        const fileName = path.parse(filePath.name).name;
+        const fileExt = path.extname(filePath.name).slice(1);
+        //Get file size in KB
+        const fileSizeKB = (stats.size / 1024).toFixed(2);
+
+        //Display file information
+        console.log(`${fileName} - ${fileExt} - ${fileSizeKB}KB`);
+      }
     }
-      
     //Error Handling
   } catch (error) {
     console.error('Error reading the file:', error);
   }
 }
+
+displayFileInfo();
