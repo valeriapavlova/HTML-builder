85f419dfcbb4eabb7a9d08184b3990495f4ca0f0
valeriapavlova <vapavlovalera@gmail.com>
2025-01-19 23:35:40 +0100

    feat: add module imports and add initial setup

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,9 @@
+//Module imports
+const fs = require('fs/promises');
+const path = require('path');
+
+async function displayFileInfo() {
+  try {
+        const folderPath = path.join(__dirname, 'secret-folder');
+    }
+}
