63a6614274fa75f54b6d7464631343a8b72fbd4f
valeriapavlova <vapavlovalera@gmail.com>
2025-01-19 21:53:13 +0100

    feat: add file and stream setup to index.js in 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,3 +2,8 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
+
+// File and stream setup
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+
