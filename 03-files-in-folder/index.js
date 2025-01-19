705cb41f1df693c4d66bfadb7f60fd133ea7cecb
valeriapavlova <vapavlovalera@gmail.com>
2025-01-19 23:46:39 +0100

    feat: read directory contents with file information

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,6 +6,9 @@ async function displayFileInfo() {
   try {
     const folderPath = path.join(__dirname, 'secret-folder');
 
+    //Readd directory contents
+    const files = await fs.readdir(folderPath, { withFileTypes: true });
+    
     //Error Handling
   } catch (error) {
     console.error('Error reading the file:', error);
