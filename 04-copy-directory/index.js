8f2b39e023e6266f0acb035a4c5e445ae7c898c6
valeriapavlova <vapavlovalera@gmail.com>
2025-01-20 13:00:12 +0100

    feat: define source and target directories in index.js in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,7 +2,8 @@ const fs = require('fs/promises');
 const path = require('path');
 
 async function copyDir() {
-
+  const sourceDir = path.join(__dirname, 'files');
+  const targetDir = path.join(__dirname, 'files-copy');
 }
 
 copyDir();
