2541d6afe4950058ae50621ed3bb1bddd87c39cc
valeriapavlova <vapavlovalera@gmail.com>
2025-01-20 13:02:53 +0100

    feat: create target directory in index.js in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,6 +4,11 @@ const path = require('path');
 async function copyDir() {
   const sourceDir = path.join(__dirname, 'files');
   const targetDir = path.join(__dirname, 'files-copy');
+
+  try {
+    //create target directory
+    await fs.mkdir(targetDir, { recursive: true });
+  }
 }
 
 copyDir();
